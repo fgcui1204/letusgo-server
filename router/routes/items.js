c045b2d391aa5747160d6a65a68f0dab3e8fe785
fgcui <fgcui1204@163.com>
2014-09-24 17:47:09 +0800

    sort use oo

--- router/routes/items.js
+++ router/routes/items.js
@@ -6,12 +6,12 @@ var redis = require('redis');
 var client = redis.createClient();
 
 var items = [
-  {barcode:'1', productSort: '水果', productName: '苹果', productPrice: '10', productUnit: '千克'},
-  {barcode:'2', productSort: '水果', productName: '香蕉', productPrice: '5', productUnit: '千克'},
-  {barcode:'3', productSort: '饮料', productName: '可乐', productPrice: '5', productUnit: '瓶'},
-  {barcode:'4', productSort: '饮料', productName: '雪碧', productPrice: '3', productUnit: '瓶'},
-  {barcode:'5', productSort: '服装', productName: 'NIKE鞋', productPrice: '300', productUnit: '双'},
-  {barcode:'6', productSort: '服装', productName: '阿迪T恤', productPrice: '200', productUnit: '件'}
+  {barcode:'1', productSort: {sid: '1', sname: '水果'}, productName: '苹果', productPrice: '10', productUnit: '千克'},
+  {barcode:'2', productSort: {sid: '1', sname: '水果'}, productName: '香蕉', productPrice: '5', productUnit: '千克'},
+  {barcode:'3', productSort: {sid: '2', sname: '饮料'}, productName: '可乐', productPrice: '5', productUnit: '瓶'},
+  {barcode:'4', productSort: {sid: '2', sname: '饮料'}, productName: '雪碧', productPrice: '3', productUnit: '瓶'},
+  {barcode:'5', productSort: {sid: '3', sname: '服装'}, productName: 'NIKE鞋', productPrice: '300', productUnit: '双'},
+  {barcode:'6', productSort: {sid: '3', sname: '服装'}, productName: '阿迪T恤', productPrice: '200', productUnit: '件'}
 ];
 client.set('items',JSON.stringify(items));
 router.get('/', function(req, res) {
