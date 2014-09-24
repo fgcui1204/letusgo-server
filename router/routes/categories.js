29ec469785c9f226a19b4b69937f698406f2b895
fgcui <fgcui1204@163.com>
2014-09-24 20:25:33 +0800

    create cartItems

--- router/routes/categories.js
+++ router/routes/categories.js
@@ -1,9 +1,22 @@
+'use strict';
 var express = require('express');
 var router = express.Router();
 
+var redis = require('redis');
+var client = redis.createClient();
+
+var sorts = [
+  {sid: '1', sname: '水果'},
+  {sid: '2', sname: '饮料'},
+  {sid: '3', sname: '服装'},
+  {sid: '4', sname: '蔬菜'}
+];
+client.set('sorts',JSON.stringify(sorts));
 router.get('/', function(req, res) {
-  //TODO: Need to implement.
-  res.send('Success!');
+  client.get('sorts',function(err,reply){
+    res.send(JSON.stringify(reply));
+  });
 });
 
 module.exports = router;
+
