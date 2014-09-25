35411dce73f4b16338947182d1223a3d87154079
fgcui <fgcui1204@163.com>
2014-09-26 00:31:01 +0800

    require lodash

--- router/routes/categories.js
+++ router/routes/categories.js
@@ -5,17 +5,43 @@ var router = express.Router();
 var redis = require('redis');
 var client = redis.createClient();
 
+var _ = require('lodash');
+
 var sorts = [
   {sid: '1', sname: '水果'},
   {sid: '2', sname: '饮料'},
   {sid: '3', sname: '服装'},
   {sid: '4', sname: '蔬菜'}
 ];
+
 client.set('sorts',JSON.stringify(sorts));
+
 router.get('/', function(req, res) {
   client.get('sorts',function(err,reply){
     res.send(reply);
   });
+});
+
+router.post('/',function(req,res){
+  var category = req.param('sort');
+  client.get('sorts', function (err, reply) {
+    var sorts = JSON.parse(reply);
+    var isTheRepeat = [];
+    _.forEach(sorts, function (sort) {
+      if (sort.sname === category.sname) {
+        isTheRepeat = sort.sname;
+      }
+    });
+    if (isTheRepeat.toString() === '') {
+      category.sid = parseInt(sorts[sorts.length - 1].sid) + 1 + '';
+      sorts.push(category);
+      client.set('sorts', JSON.stringify(sorts), function (err, reply) {
+        res.send(sorts);
+      });
+    }
+
+  });
+
 });
 
 module.exports = router;
